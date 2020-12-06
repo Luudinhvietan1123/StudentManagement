fb57fe2a1a712b9ba4581273ea559ebb744493c5
DucAnh <dinhanh21092000@gmail.com>
2020-12-06 13:19:39 +0700

    adding update feature

--- src/Sevice/updateService.java
+++ src/Sevice/updateService.java
@@ -5,10 +5,46 @@
  */
 package Sevice;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author anhaa
  */
 public class updateService {
+    public boolean checkStudentExist(int studentId) {
+       boolean checkResult = false;
+        Statement statement = null;
+        try{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  // load the driver
+        Connection connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=SinhVien;user=sa;password=123456;");
+
+        //System.out.println("Connected to database !");
     
+        statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,  
+        ResultSet.CONCUR_UPDATABLE);
+        String sql = "select * from SinhVien.dbo.Table_1";
+        ResultSet resultSet = statement.executeQuery(sql);
+        while(resultSet.next()){
+           
+            
+            
+            //System.out.println(resultSet.getString(1));
+            //System.out.println(resultSet.getString(2));
+            
+            }
+        }
+        catch(SQLException sqle) {
+        //System.out.println("Sql Exception :"+sqle.getMessage());
+        }
+        catch(ClassNotFoundException e) {
+        //System.out.println("Class Not Found Exception :" + e.getMessage());
+        }
+        
+        return checkResult;
+    }
 }
