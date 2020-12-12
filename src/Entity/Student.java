ac981939c592bd879161ef8d449c77acd22a0987
DucAnh <dinhanh21092000@gmail.com>
2020-12-12 23:41:06 +0700

    them tinh nang chinh sua ,xoa ,them

--- src/Entity/Student.java
+++ src/Entity/Student.java
@@ -5,6 +5,8 @@
  */
 package Entity;
 
+ 
+
 /**
  *
  * @author Dell
@@ -21,90 +23,136 @@ public class Student {
     private String phoneNumber;
     private boolean studentStatus;
 
+ 
+
     public String getStudentName() {
         return studentName;
     }
+
+ 
 
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
 
+ 
+
     public String getDateOfBirth() {
         return dateOfBirth;
     }
+
+ 
 
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
+ 
+
     public String getHomeTown() {
         return homeTown;
     }
+
+ 
 
     public void setHomeTown(String homeTown) {
         this.homeTown = homeTown;
     }
 
+ 
+
     public int getAcademicYear() {
         return academicYear;
     }
+
+ 
 
     public void setAcademicYear(int academicYear) {
         this.academicYear = academicYear;
     }
 
+ 
+
     public int getStudentId() {
         return studentId;
     }
+
+ 
 
     public void setStudentId(int studentId) {
         this.studentId = studentId;
     }
 
+ 
+
     public float getCPA() {
         return CPA;
     }
+
+ 
 
     public void setCPA(float CPA) {
         this.CPA = CPA;
     }
 
+ 
+
     public String getMayjor() {
         return mayjor;
     }
+
+ 
 
     public void setMayjor(String mayjor) {
         this.mayjor = mayjor;
     }
 
+ 
+
     public String getMail() {
         return mail;
     }
+
+ 
 
     public void setMail(String mail) {
         this.mail = mail;
     }
 
+ 
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+ 
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+ 
+
     public boolean isStudentStatus() {
         return studentStatus;
     }
+
+ 
 
     public void setStudentStatus(boolean studentStatus) {
         this.studentStatus = studentStatus;
     }
 
-    public Student() {
+ 
+
+    public Student(int StudentId) {
+         this.studentId = studentId;
+       
     }
 
-    public Student(String studentName, String dateOfBirth, String homeTown, int academicYear, int studentId, float CPA, String mayjor, String mail, String phoneNumber, boolean studentStatus) {
+ 
+
+    public Student(String studentName, String dateOfBirth, String homeTown, int academicYear, int studentId, float CPA, String mayjor, String mail, String phoneNumber) {
         this.studentName = studentName;
         this.dateOfBirth = dateOfBirth;
         this.homeTown = homeTown;
@@ -114,8 +162,9 @@ public class Student {
         this.mayjor = mayjor;
         this.mail = mail;
         this.phoneNumber = phoneNumber;
-        this.studentStatus = studentStatus;
     }
-    
+    // Upda1: xoa thuoc tinh status khi khoi tao
+    // allow null status trong db: ALTER TABLE Students.dbo.Students_Table ALTER COLUMN Status bit NULL;
     
 }
+ 
